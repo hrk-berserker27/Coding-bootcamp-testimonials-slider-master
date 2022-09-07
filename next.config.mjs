6a0c4c81f18cf34aaf61ed00b02dba73ee90f505
Hritick <hritickbhushan@gmail.com>
2022-09-07 16:35:31 +0530

    added some new files

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: "/Coding-bootcamp-testimonials-slider-master",
+  assetPrefix: "/Coding-bootcamp-testimonials-slider-master",
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+};
+
+export default nextConfig;
